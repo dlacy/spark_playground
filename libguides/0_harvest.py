83db1fb5b1141321d607525762e7531614f216ea
David Lacy <david.lacy@temple.edu>
2017-04-27 13:42:48 -0400

    Converted to Spark routine

--- libguides/0_harvest.py
+++ libguides/0_harvest.py
@@ -1,33 +1,64 @@
 import json
+import urllib
 import urllib.request
+from http.cookiejar import CookieJar
+import requests
 import html
 import re
+import time
+from pyspark.sql import SparkSession, Row
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
-with open('data/all_guides.json') as json_data:
-    d = json.load(json_data)
 
-# print(d)
-print(len(d))
+def get_guide(url):
 
-for guide in d:
-    print(guide["id"], guide["name"], len(guide["pages"]))
-    for page in guide["pages"]:
-        if len(page["redirect_url"]):
-            break
-        print(" - ", page["id"], page["name"])
+    import requests
 
-        url = "http://guides.temple.edu/c.php?g=%s&p=%s" % (guide["id"], page["id"])
-        print("   - ", url)
-        regex = re.compile('[^a-zA-Z]')
-        filename = regex.sub('', page["name"]) + "_" + guide["id"] + "_" + page["id"]
-        filepath = "data/guides/" + filename + ".json"
-        print("   - ", filename)
-        print("   - ", filepath)
-        response = urllib.request.urlopen(url)
-        content = response.read().decode('utf-8')
+    try:
+        response = requests.get(url, allow_redirects=False)
 
-        import json
+        if response.status_code == 200:
+            # Try to get the data and json.load it 5 times, then give up
+            tries = 5
+            while tries >= 0:
+                try:
+                    print("  ---------- trying: ", url)
+                    content = response.text
+                    return content
+                except:
+                    if tries == 0:
+                        # If we keep failing, raise the exception for the outer exception
+                        # handling to deal with
+                        raise
+                    else:
+                        # Wait a few seconds before retrying and hope the problem goes away
+                        time.sleep(3)
+                        tries -= 1
+                        continue
+    except:
+        raise
 
-        with open(filepath, 'w') as outfile:
-            json.dump([guide["id"], page["id"], content], outfile)
+spark = SparkSession.builder.appName("harvester").getOrCreate()
 
+guides = spark.read.json('data/flattened_guides.json')
+
+guides.show(10)
+
+guides.printSchema()
+
+# Define udf
+from pyspark.sql.functions import udf
+udfget_guide=udf(get_guide, StringType())
+
+guidesDF = guides.withColumn("content", udfget_guide(guides.url))
+
+print("done")
+
+# save to parquet
+guidesDF_filtered = guidesDF.rdd.filter(lambda x: x is not None)
+
+guidesDF = guidesDF_filtered.toDF()
+
+guidesDF.show(10)
+
+guidesDF.write.save("data/all_guides.parquet", format="parquet")
