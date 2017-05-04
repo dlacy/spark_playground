9bd8f4f259fd8636474c0cd9902aa910af74d643
David Lacy <david.lacy@temple.edu>
2017-05-04 14:19:30 -0400

    Added LDA clustering

--- libguides/2_tokenize_and_vectorize.py
+++ libguides/2_tokenize_and_vectorize.py
@@ -1,53 +1,61 @@
-from pyspark.ml.feature import HashingTF, IDF, Tokenizer, StopWordsRemover, CountVectorizer, RegexTokenizer
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer, StopWordsRemover, CountVectorizer, RegexTokenizer, Word2Vec
 from pyspark.sql import SparkSession
+from pyspark.ml.clustering import LDA
 
 spark = SparkSession.builder.appName("tokenizer").getOrCreate()
 
 # Loads data.
-raw = spark.read.load("data/libguides_bow.parquet")
-raw.show(10)
-#print(" ----------- raw.count()", raw.count())
+raw = spark.read.load("data/libguides_txt.parquet")
 
-guidesDFrame = raw.filter("words is not null")
-#print(" ----------- guidesDFrame.count()", guidesDFrame.count())
+# Purge Null / None datas
+nullified = raw.na.drop()
 
-# begin tokenization and other processing
+guidesDFrame = nullified.select("guide_id","guide_name","page_id","page_name","words")
 
-tokenizer = Tokenizer(inputCol="words", outputCol="bow")
+tokenizer = Tokenizer(inputCol="words", outputCol="word_tokens")
 TokenizerData = tokenizer.transform(guidesDFrame)
-TokenizerData.show(10)
 guidesDFrame = TokenizerData
 
 
 # Remove Stop Words
 # https://spark.apache.org/docs/latest/ml-features.html#stopwordsremover
-remover = StopWordsRemover(inputCol="bow", outputCol="stop_removed")
+remover = StopWordsRemover(inputCol="word_tokens", outputCol="stop_removed")
+
+my_sw = ["guide", "books", "database", "meta", "results", "https", "login", "updated", "david", "dillard", "use", "guide", "www", "search", "edu", "guides", "eric", "library", "find", "check", "doc", "check", "administration", "want", "ebsco", "http", "r", "f", "google", "com", "less", "tinyurl", "isbn", "call", "number", "date", "c", "paley", "temple", "research"]
+sw = remover.loadDefaultStopWords("english")
+remover.setStopWords(sw + my_sw)
+
 StopWordsRemoverData = remover.transform(guidesDFrame)
-StopWordsRemoverData.show(10)
 guidesDFrame = StopWordsRemoverData
 
-#https://spark.apache.org/docs/latest/ml-features.html#tf-idf
-hashingTF = HashingTF(inputCol="stop_removed", outputCol="HashingTF", numFeatures=1000)
-featurizedData = hashingTF.transform(StopWordsRemoverData)
-guidesDFrame = featurizedData
-
-#
-cv = CountVectorizer(inputCol="stop_removed", outputCol="CountVectorizer", vocabSize=1000, minDF=1.0, minTF=2.0)
+cv = CountVectorizer(inputCol="stop_removed", outputCol="CountVectorizer", vocabSize=1000, minDF=1.0, minTF=10.0)
 transformer = cv.fit(guidesDFrame)
 print(" ----------- ", transformer.vocabulary)
+
+vacabulary = transformer.vocabulary
+
 CountVectorizerData = transformer.transform(guidesDFrame)
 guidesDFrame = CountVectorizerData
 
-idf = IDF(inputCol="HashingTF", outputCol="IDF_HashingTF")
-idfModel = idf.fit(guidesDFrame)
-rescaledData = idfModel.transform(guidesDFrame)
-guidesDFrame = rescaledData
+# Trains a LDA model.
+lda = LDA(k=10, maxIter=15, featuresCol="CountVectorizer")
+model = lda.fit(guidesDFrame)
 
-idf = IDF(inputCol="CountVectorizer", outputCol="IDF_CountVectorizer")
-idfModel = idf.fit(guidesDFrame)
-rescaledData = idfModel.transform(guidesDFrame)
-guidesDFrame = rescaledData
+print("------------")
+model.vocabSize()
+print("------------")
+model.describeTopics(maxTermsPerTopic=20).show()
+topics = model.describeTopics(maxTermsPerTopic=20).collect()
+print(topics)
 
-guidesDFrame.write.save("data/guides_tokenized_and_vectorized.parquet", format="parquet")
-#guidesDFrame.select("guide_id", "page_id", "HashingTF", "CountVectorizer", "IDF_HashingTF", "IDF_CountVectorizer").show(truncate=False)
+i=0
+for topic in topics:
+    print(topic["topic"])
+    for word_id in topic["termIndices"]:
+        print(word_id, " - ", vacabulary[word_id])
+print("------------")
 
+ldaData = model.transform(guidesDFrame)
+guidesDFrame = ldaData
+
+guidesDFrame.show(10)
